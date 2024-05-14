542057be7b3221f7078d50d3ca86cd4e1f8a6f0b
Rushikesh <rushikesh.belkunde@algorisys.com>
2024-05-14 11:11:22 +0530

    test

--- app/main.py
+++ app/main.py
@@ -1,7 +1,19 @@
 # Uncomment this to pass the first stage
 # import socket
 import socket
+
 def main():
+
+
+    def getResponse(method, path):
+      
+        if path == "/":
+            return "HTTP/1.1 200 OK\r\n\r\n"
+        else:
+            return 'HTTP/1.1 404 Not Found\r\n\r\n'
+
+    GET = "GET"
+    POST = "POST"
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
     # Uncomment this to pass the first stage
@@ -12,7 +24,21 @@ def main():
     # server_socket.accept() 
     client, addr = server_socket.accept()
 
-    client.sendall('HTTP/1.1 200 OK\r\n\r\n'.encode())
+
+    lines = client.recv(4096).decode().split(' ')
+
+    print(lines)
+
+    
+    method = lines[0]
+    path = lines[1]
+    print("method and path", method, path)
+
+    response = getResponse(method, path)
+    client.sendall(response.encode())
+
+
+    # client.close()
 
     
 if __name__ == "__main__":
