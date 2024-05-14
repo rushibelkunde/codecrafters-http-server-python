1d79565ea2edcf25bfc2ac5d94f864611fc83c97
Rushikesh Belkunde <rushibelkunde18@gmail.com>
2024-05-14 22:13:59 +0530

    test

--- app/main.py
+++ app/main.py
@@ -26,7 +26,7 @@ def getResponseTxt(method, path, lines):
                        
                         return f"HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: "+ str(cont_length)+ "\r\n\r\n"+ content
                 elif method == "POST":
-                    body = lines[-2].split('\r\n\r\n')[1] + ' '.join(lines[-1])
+                    body = lines[4].split('\r\n\r\n')[1] + ' '.join(lines[5:])
                     filepath = f"{argv[2]}/{f_name}"
                     with open(body, "wb") as file:
                         file.write("".encode("utf-8"))
