bde1595f09cebb13ba54c9ffa17e0a95664e995a
Rushikesh <rushikesh.belkunde@algorisys.com>
2024-05-15 10:21:41 +0530

    test

--- app/main.py
+++ app/main.py
@@ -27,6 +27,7 @@ def parse_request(request_string):
     return method, path, http_version, headers, body
 
 def validateEncoding(encodings):
+    print(encodings)
     invalid_encodings = ['encoding-1', 'encoding-2', 'invalid_encoding']
     validateEncodings = filter(lambda encoding: encoding not in invalid_encodings, encodings)
     return list(validateEncodings)
